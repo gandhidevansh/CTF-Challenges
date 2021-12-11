466a7c505076f7959752224fb18495ad58c2a707
d3vil <31349598+gandhidevansh@users.noreply.github.com>
2021-12-11 18:40:19 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,7 @@
-function typeeffect() {
   var c = 0;
   var txt = '< d3vil CTF />';
+
+function typeeffect() {
   if (c < txt.length) {
     document.getElementById("title").innerHTML += txt.charAt(i) ;
     c++;
